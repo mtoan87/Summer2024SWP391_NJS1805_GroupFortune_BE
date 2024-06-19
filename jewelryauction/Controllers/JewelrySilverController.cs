425401115b96eccaf0ecab05304cdc1323fa7388
mtoan87 <122218541+mtoan87@users.noreply.github.com>
2024-06-19 17:19:41 +0700

    update jewsstaff funct

--- jewelryauction/Controllers/JewelrySilverController.cs
+++ jewelryauction/Controllers/JewelrySilverController.cs
@@ -84,6 +84,29 @@ namespace jewelryauction.Controllers
             return Ok(rs);
         }
 
+        [HttpPut]
+        [Route("UpdateJewelrySilverStaff")]
+        public async Task<IActionResult> UpdateSilverJewelryStaff(int id, [FromForm] UpdateJewelrySilverStaffDTO updateJewelry, IFormFile jewelryImg)
+        {
+            if (jewelryImg != null)
+            {
+                var folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "assets");
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+                var filePath = Path.Combine(folderPath, jewelryImg.FileName);
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await jewelryImg.CopyToAsync(stream);
+                }
+                updateJewelry.JewelryImg = $"assets/{jewelryImg.FileName}";
+            }
+
+            var rs = await _jewelrySilverService.UpdateJewelryStaff(id, updateJewelry);
+            return Ok(rs);
+        }
+
         [HttpDelete]
         [Route("DeleteJewelrySilver")]
         public async Task<IActionResult> DeleteSilverJewelry(int id)
