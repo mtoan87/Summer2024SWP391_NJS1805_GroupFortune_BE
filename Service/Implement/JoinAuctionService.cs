8e80c41b896c76a6ac562ce18ae79cf59cb6a556
mtoan87 <122218541+mtoan87@users.noreply.github.com>
2024-06-25 15:44:37 +0700

    updated JoinAuctionwithValidateAccountBudget

--- Service/Implement/JoinAuctionService.cs
+++ Service/Implement/JoinAuctionService.cs
@@ -15,19 +15,38 @@ namespace Service.Implement
     public class JoinAuctionService
     {
         private readonly JoinAuctionRepository _joinAuctionRepository;
-        public JoinAuctionService(JoinAuctionRepository joinAuctionRepository)
+        private readonly AuctionRepository _auctionRepository;
+        private readonly JewelryGoldRepository _jewelryGoldRepository;
+        private readonly JewelrySilverRepository _jewelrySilverRepository;
+        private readonly JewelryGoldDiaRepository _jewelryGoldDiaRepository;
+        private readonly AccountWalletRepository _accountWalletRepository;
+
+        public JoinAuctionService(
+            JoinAuctionRepository joinAuctionRepository,
+            AuctionRepository auctionRepository,
+            JewelryGoldRepository jewelryGoldRepository,
+            JewelrySilverRepository jewelrySilverRepository,
+            JewelryGoldDiaRepository jewelryGoldDiaRepository,
+            AccountWalletRepository accountWalletRepository)
         {
             _joinAuctionRepository = joinAuctionRepository;
+            _auctionRepository = auctionRepository;
+            _jewelryGoldRepository = jewelryGoldRepository;
+            _jewelrySilverRepository = jewelrySilverRepository;
+            _jewelryGoldDiaRepository = jewelryGoldDiaRepository;
+            _accountWalletRepository = accountWalletRepository;
         }
+
         public async Task<IEnumerable<JoinAuction>> GetAllJoinAuctions()
         {
-            return  await _joinAuctionRepository.GetAllAsync();
+            return await _joinAuctionRepository.GetAllAsync();
         }
 
         public async Task<JoinAuction> GetJoinAuctionById(int id)
         {
             return await _joinAuctionRepository.GetByIdAsync(id);
         }
+
         public async Task<JoinAuction> CreateJoinAuction(CreateJoinAuctionDTO createJoinAuction)
         {
             var newJoinAuction = new JoinAuction
@@ -37,9 +56,60 @@ namespace Service.Implement
                 AuctionId = createJoinAuction.AuctionId,
                 Joindate = DateTime.Now,
             };
-            await _joinAuctionRepository.AddAsync(newJoinAuction);
-            
-            return newJoinAuction;
+
+            if (await CanJoinAuction(newJoinAuction.AccountId.Value, newJoinAuction.AuctionId.Value))
+            {
+                await _joinAuctionRepository.AddAsync(newJoinAuction);
+                return newJoinAuction;
+            }
+            else
+            {
+                throw new Exception("Insufficient budget to join the auction.");
+            }
+        }
+
+        public async Task<bool> CanJoinAuction(int accountId, int auctionId)
+        {
+            var accountWallet = await _accountWalletRepository.GetByIdAsync(accountId);
+            if (accountWallet == null)
+            {
+                throw new Exception("Account wallet not found.");
+            }
+
+            var auction = await _auctionRepository.GetByIdAsync(auctionId);
+            if (auction == null)
+            {
+                throw new Exception("Auction not found.");
+            }
+
+            double? jewelryPrice = null;
+
+            if (auction.JewelryGoldId != null)
+            {
+                var jewelryGold = await _jewelryGoldRepository.GetByIdAsync(auction.JewelryGoldId.Value);
+                jewelryPrice = jewelryGold?.Price;
+            }
+            else if (auction.JewelryGolddiaId != null)
+            {
+                var jewelryGoldDiamond = await _jewelryGoldDiaRepository.GetByIdAsync(auction.JewelryGolddiaId.Value);
+                jewelryPrice = jewelryGoldDiamond?.Price;
+            }
+            else if (auction.JewelrySilverId != null)
+            {
+                var jewelrySilver = await _jewelrySilverRepository.GetByIdAsync(auction.JewelrySilverId.Value);
+                jewelryPrice = jewelrySilver?.Price;
+            }
+            else
+            {
+                throw new Exception("Invalid auction configuration: No associated jewelry.");
+            }
+
+            if (jewelryPrice == null)
+            {
+                throw new Exception("Jewelry not found or invalid price.");
+            }
+
+            return accountWallet.Budget > jewelryPrice;
         }
 
         public async Task<JoinAuction> UpdateJoinAuction(int id, UpdateJoinAuctionDTO updateJoinAuction)
@@ -53,19 +123,20 @@ namespace Service.Implement
             auction.AuctionId = updateJoinAuction.AuctionId;
             auction.AccountId = updateJoinAuction.AccountId;
             auction.BidId = updateJoinAuction.BidId;
-             await _joinAuctionRepository.UpdateAsync(auction);
+            await _joinAuctionRepository.UpdateAsync(auction);
             return auction;
         }
 
         public async Task<JoinAuction> DeleteJoinAuction(int id)
         {
-            var account = await _joinAuctionRepository.GetByIdAsync(id);
-            if (account == null)
+            var auction = await _joinAuctionRepository.GetByIdAsync(id);
+            if (auction == null)
             {
-                throw new Exception($"Account with ID {id} not found.");
+                throw new Exception($"JoinAuction with ID {id} not found.");
             }
-            await _joinAuctionRepository.RemoveAsync(account);
-            return account;
+            await _joinAuctionRepository.RemoveAsync(auction);
+            return auction;
         }
     }
 }
+
