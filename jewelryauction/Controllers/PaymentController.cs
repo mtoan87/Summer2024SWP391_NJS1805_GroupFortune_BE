f8808357be3e9da1b61181eef183d47726433a72
mtoan87 <122218541+mtoan87@users.noreply.github.com>
2024-06-25 17:28:38 +0700

    updated CreatePayment

--- jewelryauction/Controllers/PaymentController.cs
+++ jewelryauction/Controllers/PaymentController.cs
@@ -34,7 +34,13 @@ namespace jewelryauction.Controllers
             }
             return Ok(rs);
         }
-        
+        [HttpPost]
+        [Route("CreatePayment")]
+        public async Task<IActionResult> CreatePayment(CreatePaymentDTO createPayment)
+        {
+            var rs = await _service.CreatePayment(createPayment);
+            return Ok(rs);
+        }
 
     }
 }
