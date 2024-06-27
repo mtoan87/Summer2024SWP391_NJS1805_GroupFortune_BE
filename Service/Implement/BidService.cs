5ef621b6fc4053da65d44b76187f0de8c23a4e9f
mtoan87 <122218541+mtoan87@users.noreply.github.com>
2024-06-27 13:24:25 +0700

    revert bidding

--- Service/Implement/BidService.cs
+++ Service/Implement/BidService.cs
@@ -1,42 +1,31 @@
-﻿// Make sure to install RabbitMQ.Client NuGet package
-// Install-Package RabbitMQ.Client
-
+﻿using DAL.DTO.AuctionDTO;
+using DAL.DTO.AuctionResultDTO;
 using DAL.DTO.BidDTO;
 using DAL.Models;
-using RabbitMQ.Client;
 using Repository.Implement;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Service.Implement
 {
-    public class BidService : IDisposable
+    public class BidService
     {
-        private readonly IModel _channel;
-        private const string ExchangeName = "bids";
         private readonly BidRepository _bidRepository;
         private readonly JewelryGoldRepository _jewelryGoldRepository;
         private readonly JewelrySilverRepository _jewelrySilverRepository;
         private readonly JewelryGoldDiaRepository _jewelryGoldDiaRepository;
         private readonly BidRecordRepository _bidRecordRepository;
-
         public BidService(BidRepository bidRepository, JewelryGoldRepository jewelryGoldRepository, JewelrySilverRepository jewelrySilverRepository, JewelryGoldDiaRepository jewelryGoldDiaRepository, BidRecordRepository bidRecordRepository)
         {
-            _bidRepository = bidRepository ?? throw new ArgumentNullException(nameof(bidRepository));
-            _jewelryGoldRepository = jewelryGoldRepository ?? throw new ArgumentNullException(nameof(jewelryGoldRepository));
-            _jewelrySilverRepository = jewelrySilverRepository ?? throw new ArgumentNullException(nameof(jewelrySilverRepository));
-            _jewelryGoldDiaRepository = jewelryGoldDiaRepository ?? throw new ArgumentNullException(nameof(jewelryGoldDiaRepository));
-            _bidRecordRepository = bidRecordRepository ?? throw new ArgumentNullException(nameof(bidRecordRepository));
-
-            var factory = new ConnectionFactory() { HostName = "localhost" };
-            var connection = factory.CreateConnection();
-            _channel = connection.CreateModel();
-
-            _channel.ExchangeDeclare(ExchangeName, ExchangeType.Fanout);
+            _jewelryGoldRepository = jewelryGoldRepository;
+            _jewelrySilverRepository = jewelrySilverRepository;
+            _jewelryGoldDiaRepository = jewelryGoldDiaRepository;
+            _bidRecordRepository = bidRecordRepository;
+            _bidRepository = bidRepository;
         }
-
         public async Task<IEnumerable<Bid>> GetBidByAccountIdAsync(int accountId)
         {
             return await _bidRepository.GetBidByAccountId(accountId);
@@ -54,7 +43,7 @@ namespace Service.Implement
                 AccountId = createBid.AccountId,
                 AuctionId = createBid.AuctionId,
                 Minprice = createBid.Minprice,
-                Maxprice = createBid.Minprice,
+                Maxprice = createBid.Maxprice,
                 Datetime = DateTime.Now,
             };
             await _bidRepository.AddAsync(newBid);
@@ -62,40 +51,58 @@ namespace Service.Implement
             return newBid;
         }
 
-        public async Task<Bid> UpdateBid(int id, UpdateBidDTO updateBid)
+        public async Task<Bid> UpdateBid(int id,UpdateBidDTO updateBid)
         {
             var bid = await _bidRepository.GetByIdAsync(id);
-            if (bid == null)
+            if(bid == null)
             {
-                throw new Exception($"Bid with ID {id} not found");
+                throw new Exception($"Bid with ID{id} not found");
             }
             bid.AuctionId = updateBid.AuctionId;
             bid.AccountId = updateBid.AccountId;
             bid.Minprice = updateBid.Minprice;
             bid.Maxprice = updateBid.Maxprice;
             await _bidRepository.UpdateAsync(bid);
-            await _bidRepository.SaveChangesAsync();
-
+           
             return bid;
-        }
 
+        }
         public async Task<bool> PlaceBid(BiddingDTO bidDto)
         {
-           
-            var newMaxPrice = await CalculateNewMaxPrice(bidDto);
+            var jewelryGold = await _jewelryGoldRepository.GetByIdAsync(bidDto.AuctionId);
+            var jewelryGoldDiamond = await _jewelryGoldDiaRepository.GetByIdAsync(bidDto.AuctionId);
+            var jewelrySilver = await _jewelrySilverRepository.GetByIdAsync(bidDto.AuctionId);
 
-            
+            double minPrice = 0;
+            if (jewelryGold != null)
+            {
+                minPrice = jewelryGold.Price ?? 0;
+            }
+            else if (jewelryGoldDiamond != null)
+            {
+                minPrice = jewelryGoldDiamond.Price ?? 0;
+            }
+            else if (jewelrySilver != null)
+            {
+                minPrice = jewelrySilver.Price ?? 0;
+            }
+            else
+            {
+                return false;
+            }
+
             var existingBid = await _bidRepository.GetByAccountIdAndAuctionId(bidDto.AccountId, bidDto.AuctionId);
 
+            double newMaxPrice;
             if (existingBid == null)
             {
-                var minPrice = newMaxPrice - bidDto.BidStep;
+                newMaxPrice = minPrice + bidDto.BidStep;
                 var newBid = new Bid
                 {
                     AccountId = bidDto.AccountId,
                     AuctionId = bidDto.AuctionId,
                     Minprice = minPrice,
-                    Maxprice = newMaxPrice,
+                    Maxprice = minPrice,  // Ensuring Maxprice is initialized to Minprice
                     Datetime = DateTime.Now
                 };
 
@@ -125,70 +132,13 @@ namespace Service.Implement
                 };
 
                 await _bidRecordRepository.AddAsync(bidRecord);
-                await _bidRepository.SaveChangesAsync();
             }
 
-            
-            var bidUpdate = new
-            {
-                AuctionId = bidDto.AuctionId,
-                NewMaxPrice = newMaxPrice
-            };
-
-            var message = JsonSerializer.Serialize(bidUpdate);
-            var body = Encoding.UTF8.GetBytes(message);
-
-            _channel.BasicPublish(exchange: ExchangeName,
-                                  routingKey: "",
-                                  basicProperties: null,
-                                  body: body);
+            await _bidRepository.SaveChangesAsync();
+            await _bidRecordRepository.SaveChangesAsync();
 
             return true;
         }
 
-        private async Task<double> CalculateNewMaxPrice(BiddingDTO bidDto)
-        {
-            var jewelryGold = await _jewelryGoldRepository.GetByIdAsync(bidDto.AuctionId);
-            var jewelryGoldDiamond = await _jewelryGoldDiaRepository.GetByIdAsync(bidDto.AuctionId);
-            var jewelrySilver = await _jewelrySilverRepository.GetByIdAsync(bidDto.AuctionId);
-
-            double minPrice = 0;
-            if (jewelryGold != null)
-            {
-                minPrice = jewelryGold.Price ?? 0;
-            }
-            else if (jewelryGoldDiamond != null)
-            {
-                minPrice = jewelryGoldDiamond.Price ?? 0;
-            }
-            else if (jewelrySilver != null)
-            {
-                minPrice = jewelrySilver.Price ?? 0;
-            }
-            else
-            {
-                throw new Exception($"Auction with ID {bidDto.AuctionId} not found");
-            }
-
-            var existingBid = await _bidRepository.GetByAccountIdAndAuctionId(bidDto.AccountId, bidDto.AuctionId);
-
-            double newMaxPrice;
-            if (existingBid == null)
-            {
-                newMaxPrice = minPrice + bidDto.BidStep;
-            }
-            else
-            {
-                newMaxPrice = existingBid.Maxprice + bidDto.BidStep;
-            }
-
-            return newMaxPrice;
-        }
-
-        public void Dispose()
-        {
-            _channel.Close();
-            _channel.Dispose();
-        }
     }
 }
