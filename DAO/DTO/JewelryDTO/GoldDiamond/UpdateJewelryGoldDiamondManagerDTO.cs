73335b99e739bfdbf0cd2fa253023b8ced0807b1
mtoan87 <122218541+mtoan87@users.noreply.github.com>
2024-07-09 08:16:14 +0700

    updated dto

--- DAO/DTO/JewelryDTO/GoldDiamond/UpdateJewelryGoldDiamondManagerDTO.cs
+++ DAO/DTO/JewelryDTO/GoldDiamond/UpdateJewelryGoldDiamondManagerDTO.cs
@@ -10,22 +10,22 @@ namespace DAL.DTO.JewelryDTO.GoldDiamond
 {
     public class UpdateJewelryGoldDiamondManagerDTO
     {
-        public int? AccountId { get; set; }
-        public string? JewelryImg { get; set; }
-        public string Name { get; set; } = null!;
-        [EnumDataType(typeof(Category))]
-        public string Category { get; set; } = null!;
-        [EnumDataType(typeof(Material))]
-        public string Materials { get; set; } = null!;
-        public string Description { get; set; } = null!;
-        [EnumDataType(typeof(DiamondClarity))]
-        public string Clarity { get; set; } = null!;
-        public string Carat { get; set; } = null!;
-        [EnumDataType(typeof(GoldAge))]
-        public string GoldAge { get; set; } = null!;
+        //public int? AccountId { get; set; }
+        //public string? JewelryImg { get; set; }
+        //public string Name { get; set; } = null!;
+        //[EnumDataType(typeof(Category))]
+        //public string Category { get; set; } = null!;
+        //[EnumDataType(typeof(Material))]
+        //public string Materials { get; set; } = null!;
+        //public string Description { get; set; } = null!;
+        //[EnumDataType(typeof(DiamondClarity))]
+        //public string Clarity { get; set; } = null!;
+        //public string Carat { get; set; } = null!;
+        //[EnumDataType(typeof(GoldAge))]
+        //public string GoldAge { get; set; } = null!;
 
         public double? Price { get; set; }
-        public string Weight { get; set; } = null!;
+        //public string Weight { get; set; } = null!;
         [EnumDataType(typeof(JewelryStatus))]
         public string? Status { get; set; }
     }
