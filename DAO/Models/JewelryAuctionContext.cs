024bd3a96d229c433f36704d06711578610fc527
mtoan87 <122218541+mtoan87@users.noreply.github.com>
2024-07-09 19:34:27 +0700

    fix connectionstring

--- DAO/Models/JewelryAuctionContext.cs
+++ DAO/Models/JewelryAuctionContext.cs
@@ -34,7 +34,7 @@ namespace DAL.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=LAPTOP-60V3OC09\\TOANHM;uid=sa;pwd=12345;database=JewelryAuction;TrustServerCertificate=True;");
+                optionsBuilder.UseSqlServer("Server=(local);uid=sa;pwd=12345;database=JewelryAuction;TrustServerCertificate=True;");
             }
         }
 
